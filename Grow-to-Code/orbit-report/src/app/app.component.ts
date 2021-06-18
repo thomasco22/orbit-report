9f9c4228fd5dfda5931518bfdaa9f0d5040cc468
tcolli214 <thomas_collins@cable.comcast.com>
2021-06-18 13:02:25 -0400

    saving so far

--- Grow-to-Code/orbit-report/src/app/app.component.ts
+++ Grow-to-Code/orbit-report/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Satellite } from './satellite';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'orbit-report';
+
+  sourceList: Satellite[];
+
+  constructor() {
+    this.sourceList = [];
+    let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
+ 
+    window.fetch(satellitesUrl).then(function(response) {
+       response.json().then(function(data) {
+ 
+          let fetchedSatellites = data.satellites;
+          // TODO: loop over satellites
+          for (let i = 0; i < fetchedSatellites.length; i++) {
+            let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+            this.sourceList.push(satellite);
+
+          }
+          // TODO: create a Satellite object using new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
+          // TODO: add the new Satellite object to sourceList using: this.sourceList.push(satellite);
+ 
+       }.bind(this));
+    }.bind(this));
+ 
+ }
+
+
+
+
+
 }
